0e1c1b75c942eb8fc1009bf12f168246e7d776f3
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-12-22 19:43:02 +0600

    Delete src directory

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://backendnodejs-production-8ed1.up.railway.app',
-});
-
-export default api;
--- src/views/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-      <h1>Welcome to the Diary App</h1>
-    </div>
-  </template>
